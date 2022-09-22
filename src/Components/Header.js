458bfe268dfa8136dbfab3949e8929f36da42a2b
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-09-21 21:56:50 -0300

    updated header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,19 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
+const onClick = () => {
+  console.log('click')
+}
+
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button
+        color="darkgreen"
+        text="Add"
+        onClick={onClick}
+      />
     </header>
   );
 };
