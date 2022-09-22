41646a8af15a262a12722fea5f28f3e535772cdd
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-09-21 21:56:30 -0300

    updated App with data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import Header from "./Components/Header";
+import Tasks from "./Components/Tasks";
+
 function App() {
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5 at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Movies",
+      day: "Feb 7 at 8pm",
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: "Arcade Day",
+      day: "Feb 20 at 1:30pm",
+      reminder: false,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
